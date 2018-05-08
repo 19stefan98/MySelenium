0897e053703d52bad410c6d74c00e770ba84281b
s.mitrofanov <s.mitrofanov@ITECH101>
2018-05-08 16:14:36 +0400

    fis

--- MySelenium/Program.cs
+++ MySelenium/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
 
 namespace MySelenium
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public IWebDriver driver;
+        TimeSpan timeuot = new TimeSpan(00, 01, 00);
+
+        public static void Main()
         {
+            Program ro = new Program();
+            ro.Start();
+            ro.Quit();
+        }
+
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            TimeSpan timeout = new TimeSpan(00, 00, 30);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://tairai.itech-test.ru/");
+        }
+
+        public void Quit()
+        {
+            driver.Quit();
+        }
+
+        public void Size()
+        {
+            int size = new IWindow.size(1900, 1380);
         }
     }
 }
